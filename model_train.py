fa4f0e6722d9e46462a8170bf4d98d8c73b6e6f2
intelligentgoldfish <50779300+intelligentgoldfish@users.noreply.github.com>
2020-04-19 22:08:58 -0500

    Delete model_train.py

--- model_train.py
+++
@@ -1,5 +0,0 @@
-#compile and train model
-
-model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-
-history = model.fit(imtrain, labtrain, epochs=20, validation_data=(imtest, labtest))
